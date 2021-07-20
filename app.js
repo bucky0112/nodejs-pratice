332fc300eb53e9157bc28f9af4b2d18c2a8f89cd
bucky0112 <bucky0112@gmail.com>
2021-07-20 23:57:51 +0800

    async read and write

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const fs = require('fs')
 
-const readText = fs.readFileSync('./text/about_nodejs.txt', 'utf-8')
-
+// Blocking, synchronous way
+/* const readText = fs.readFileSync('./text/about_nodejs.txt', 'utf-8'
 console.log(readText)
-
 const textOut = `以下是關於 node.js：${readText}\nmade by Bucky Chu, created on ${Date.now()}`
-
 fs.writeFileSync('./text/output.txt', textOut)
+console.log('File written') */
 
-console.log('File written')
+// Non-Blocking, asynchronous way
+fs.readFile('./text/test.txt', 'utf-8', (err, data1) => {
+  // if (err) return console.log(err)
+  fs.readFile(`./text/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2)
+    fs.readFile(`./text/output.txt`, 'utf-8', (err, data3) => {
+      console.log(data3)
+
+      fs.writeFile('./text/newOne.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Already written!')
+      })
+    })
+  })
+})
+
+console.log('This first')
