580dd37ec03a97f8b6edd0fcbbc7d90570d30ffa
bucky0112 <bucky0112@gmail.com>
2021-07-22 22:58:13 +0800

    building api

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 
 // make serve
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
   const pathName = req.url
   // console.log(req.url)
@@ -9,6 +13,19 @@ const server = http.createServer((req, res) => {
     res.end('This is overview')
   } else if (pathName === '/product') {
     res.end('This is product')
+  } else if (pathName === '/api') {
+    /* fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data)
+      console.log(productData)
+      res.writeHead(200, {
+        'Content-type': 'application/json'
+      })
+      res.end(data)
+    }) */
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    })
+    res.end(data)
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
